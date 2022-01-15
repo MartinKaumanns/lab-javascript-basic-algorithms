caafe168c9f68cbfe82946c91c6fe96902745419
Martin <martin.kaumanns@diebrueder.com>
2022-01-15 17:18:15 +0100

    done 1st Iteration

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 // Iteration 1: Names and Input
 
+const hacker1 = 'Leni';
+const hacker2 = 'Karla';
+
+let hackers = [hacker1, hacker2];
+
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
 
 
